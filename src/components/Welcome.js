d7a958e82c7850a168f2a4a9ff3d50ceefd9b9fd
Anže Hvasti <ahvasti@gmail.com>
2022-09-27 19:18:19 +0200

    removed SVG's

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -12,35 +12,6 @@ function Welcome() {
       <button onClick={playGame} className="btn btn-warning text-white my-3">
         Play
       </button>
-
-      <img
-        className="top-left"
-        src={process.env.PUBLIC_URL + "img/cross1.svg"}
-      />
-      <img
-        className="bottom-right"
-        src={process.env.PUBLIC_URL + "img/cross2.svg"}
-      />
-      <img
-        className="bottom-left"
-        src={process.env.PUBLIC_URL + "img/o1.svg"}
-      />
-      <img
-        className="cross-btm"
-        src={process.env.PUBLIC_URL + "img/cross-btm.svg"}
-      />
-      <img
-        className="cross-btm-line"
-        src={process.env.PUBLIC_URL + "img/cross-btm-line.svg"}
-      />
-      <img
-        className="o-top-line"
-        src={process.env.PUBLIC_URL + "img/o-top-line.svg"}
-      />
-      <img
-        className="x-top-line"
-        src={process.env.PUBLIC_URL + "img/x-top-line.svg"}
-      />
     </div>
   );
 }
